345dcef27bf3abcefe3a2db9f85e2c611eb4faa8
Dustin M. Eastway <dustin.eastway@gmail.com>
2019-07-20 08:27:00 -0400

    feat: add bombs and start game over

--- src/app/game-scene.ts
+++ src/app/game-scene.ts
@@ -1,4 +1,5 @@
-import { GameObjects, Physics, Scene, Types } from 'phaser';
+import { GameObjects, Input, Physics, Scene, Types } from 'phaser';
+import { gameConfig } from './game-config';
 
 enum FileKeys {
 	Bomb = 'bomb',
@@ -14,29 +15,43 @@ type PhysicsSprite = Physics.Arcade.Sprite;
 type Group = Physics.Arcade.Group;
 type StaticGroup = Physics.Arcade.StaticGroup;
 type Text = GameObjects.Text;
+const KeyCodes = Input.Keyboard.KeyCodes;
 
 const playerSpeed = 160;
 
 export class GameScene extends Scene {
+	protected _bombs: Group;
 	protected _cursorKeys: CursorKeys;
-	protected _platforms: StaticGroup;
+	protected _gameOver: boolean;
 	protected _player: PhysicsSprite;
-	protected _score = 0;
+	protected _score: number;
 	protected _scoreText: Text;
 	protected _stars: Group;
 
+	protected get score(): number {
+		return this._score;
+	}
+	protected set score(score: number) {
+		this._score = score;
+		this._scoreText.setText(`Score: ${this._score}`);
+	}
+
 	create(): void {
 		this.add.image(0, 0, FileKeys.Sky).setOrigin(0, 0);
-		this.createPlatforms();
-		this.createPlayer();
-		this.createStars();
-		this._scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
 
-		this.physics.add.collider(this._player, this._platforms);
-		this.physics.add.collider(this._stars, this._platforms);
-		this.physics.add.overlap(this._player, this._stars, this.collectStar, null, this);
+		const platforms = this.createPlatforms();
+		this._player = this.createPlayer(platforms);
+		this._bombs = this.createBombs(platforms, this._player);
+		this._stars = this.createStars(platforms, this._player);
+		this._scoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#000' });
+		this.startGameOver();
 
 		this._cursorKeys = this.input.keyboard.createCursorKeys();
+		this.input.keyboard.addListener('keydown', (event: KeyboardEvent) => {
+			if (event.keyCode === KeyCodes.ESC) {
+				this.startGameOver();
+			}
+		});
 	}
 
 	preload(): void {
@@ -55,26 +70,49 @@ export class GameScene extends Scene {
 		this.updatePlayer();
 	}
 
-	protected collectStar(player: PhysicsSprite, star: PhysicsSprite): void {
-		star.disableBody(true, true);
-		this._scoreText.setText(`Score: ${++this._score}`);
+	protected startGameOver(): void {
+		this.score = 0;
+		this._gameOver = false;
+		this._player.setPosition(100, 450);
+		this._player.setTint();
+		this._bombs.clear(true, true);
+		this.resetStars();
+		this.physics.resume();
 	}
 
-	protected createPlatforms(): void {
+	protected collectStar(player: PhysicsSprite, collectedStar: PhysicsSprite): void {
+		collectedStar.disableBody(true, true);
+		this._scoreText.setText(`Score: ${++this._score}`);
+
+		if (this._stars.countActive(true) === 0) {
+			this.resetStars();
+			this.dropBomb(player);
+		}
+	}
+
+	protected createBombs(platforms: StaticGroup, player: PhysicsSprite): Group {
+		const bombs = this.physics.add.group();
+		this.physics.add.collider(bombs, platforms);
+		this.physics.add.collider(player, bombs, this.hitBomb, null, this);
+
+		return bombs;
+	}
+
+	protected createPlatforms(): StaticGroup {
 		const platforms = this.physics.add.staticGroup();
 		platforms.create(400, 568, FileKeys.Ground).setScale(2).refreshBody();
 		platforms.create(600, 400, FileKeys.Ground);
 		platforms.create(50, 250, FileKeys.Ground);
 		platforms.create(750, 220, FileKeys.Ground);
 
-		this._platforms = platforms;
+		return platforms;
 	}
 
-	protected createPlayer(): void {
-		const player = this.physics.add.sprite(100, 450, FileKeys.Player);
+	protected createPlayer(platforms: StaticGroup): PhysicsSprite {
+		const player = this.physics.add.sprite(0, 0, FileKeys.Player);
 		player.setBounce(0.2);
 		player.setCollideWorldBounds(true);
-		this._player = player;
+		this.physics.add.collider(player, platforms);
 
 		this.anims.create({
 			key: 'left',
@@ -95,21 +133,56 @@ export class GameScene extends Scene {
 			frameRate: 10,
 			repeat: -1
 		});
+
+		return player;
 	}
 
-	protected createStars(): void {
-		this._stars = this.physics.add.group({
+	protected createStars(platforms: StaticGroup, player: PhysicsSprite): Group {
+		const stars = this.physics.add.group({
 			key: FileKeys.Star,
 			repeat: 11,
 			setXY: { x: 12, y: 0, stepX: 70 }
 		});
 
-		this._stars.children.iterate((star: PhysicsSprite) => {
+		this.physics.add.collider(stars, platforms);
+		this.physics.add.overlap(player, stars, this.collectStar, null, this);
+
+		stars.children.iterate((star: PhysicsSprite) => {
 			star.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+		});
+
+		return stars;
+	}
+
+	protected dropBomb(player: PhysicsSprite): void {
+		const width = parseInt(<string>gameConfig.width, 10);
+		const quarterWidth = width / 4;
+		const bombX = Phaser.Math.Between(player.x + quarterWidth, player.x + 3 * quarterWidth) % width;
+
+		const bomb = this._bombs.create(bombX, 16, FileKeys.Bomb);
+		bomb.setBounce(1);
+		bomb.setCollideWorldBounds(true);
+		bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+	}
+
+	protected hitBomb(player: PhysicsSprite, bomb: PhysicsSprite): void {
+		this.physics.pause();
+		player.setTint(0xff0000);
+		player.anims.play('turn');
+		this._gameOver = true;
+	}
+
+	protected resetStars(): void {
+		this._stars.children.iterate((star: PhysicsSprite) => {
+			star.enableBody(true, star.x, 0, true, true);
 		});
 	}
 
 	protected updatePlayer(): void {
+		if (this._gameOver) {
+			return;
+		}
+
 		if (this._cursorKeys.left.isDown) {
 			this._player.setVelocityX(-playerSpeed);
 			this._player.anims.play('right', true);
